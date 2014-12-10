bbacee63debe457058c1214cefa84884f770c62f
Dane Natoli <dane.natoli@gmail.com>
2014-12-10 15:51:42 +1100

    Add fallback for make and model.

--- image_data_processor/image.rb
+++ image_data_processor/image.rb
@@ -1,6 +1,8 @@
 # This class is the model representation for
 # the image data. It takes a hash and provides
-# the data through accessors.
+# the data through accessors. It returns "Unknown"
+# if the data is empty so that we can categorize
+# all photos.
 
 module ImageDataProcessor
   class Image
@@ -12,7 +14,21 @@ module ImageDataProcessor
       @thumbnail_url = attrs[:thumbnail_url]
     end
 
-    attr_reader :id, :thumbnail_url, :make, :model
+    attr_reader :id, :thumbnail_url
+
+    [:make, :model].each do |attr|
+      define_method(attr) do
+        #TODO: This is a bit nasty but not sure how to avoid the repetition
+        instance_variable = self.instance_variable_get("@#{attr}")
+        blank?(instance_variable) ? "Unknown" : instance_variable
+      end
+    end
+
+    private
+
+    def blank?(value)
+      value.respond_to?(:empty?) ? !!value.empty? : !value
+    end
 
   end
 end
--- spec/image_data_processor/image_spec.rb
+++ spec/image_data_processor/image_spec.rb
@@ -3,22 +3,44 @@ require 'spec_helper'
 RSpec.describe ImageDataProcessor::Image do
 
   describe 'attributes' do
-    let(:valid_attributes) do
-      {
-        id: "123",
-        make: "Canon",
-        model: "Powershot D50",
-        thumbnail_url: "http://example.com/really-awesome-photo"
-      }
+    context "when passed full data" do
+      let(:valid_attributes) do
+        {
+          id: "123",
+          make: "Canon",
+          model: "Powershot D50",
+          thumbnail_url: "http://example.com/really-awesome-photo"
+        }
+      end
+
+      it "sets the correct attributes" do
+        image = described_class.new valid_attributes
+
+        expect(image.id).to eq "123"
+        expect(image.make).to eq "Canon"
+        expect(image.model).to eq "Powershot D50"
+        expect(image.thumbnail_url).to eq "http://example.com/really-awesome-photo"
+      end
     end
 
-    it "sets the correct attributes" do
-      image = described_class.new valid_attributes
+    context "when passed an id and thumbnail" do
+      let(:valid_attributes) do
+        {
+          id: "123",
+          make: "",
+          model: nil,
+          thumbnail_url: "http://example.com/really-awesome-photo"
+        }
+      end
 
-      expect(image.id).to eq "123"
-      expect(image.make).to eq "Canon"
-      expect(image.model).to eq "Powershot D50"
-      expect(image.thumbnail_url).to eq "http://example.com/really-awesome-photo"
+      it "sets the correct attributes" do
+        image = described_class.new valid_attributes
+
+        expect(image.id).to eq "123"
+        expect(image.make).to eq "Unknown"
+        expect(image.model).to eq "Unknown"
+        expect(image.thumbnail_url).to eq "http://example.com/really-awesome-photo"
+      end
     end
   end
 
